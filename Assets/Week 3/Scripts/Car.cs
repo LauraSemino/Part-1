b4442bdfa1f685eadf389ecf8d2baf3ffd307c77
Laura Semino <seminol@shernet.sheridancollege.ca>
2024-01-25 14:51:33 -0500

    Task 3 Done: Car Functions

---
+++ Assets/Week 3/Scripts/Car.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    float ForwardInput;
+    float SteeringInput;
+    public float forwardSpeed = 500;
+    public float steeringSpeed = 200;
+    public float maxSpeed = 600;
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForwardInput = Input.GetAxis("Vertical");
+        SteeringInput = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddTorque(SteeringInput * -steeringSpeed * Time.deltaTime);
+        Vector2 force = (transform.up * ForwardInput * forwardSpeed * Time.deltaTime);
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        { 
+        rigidbody.AddForce(force);
+        }
+    }
+}
